62a3a8e85850953ba654f7324c611a75bcc70602
mohamedabdelfattah322-spec <mohamedabdelfattah322@gmail.com>
2026-05-18 20:17:44 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  typescript: {
+const nextConfig = {
+  typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
   images: {
     remotePatterns: [
